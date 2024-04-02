0e7183a6d70ed2ae225aab099737b1f2d688a4c2
gsrija <gsrija@LIN-5CG3202BWR.corp.capgemini.com>
2024-04-02 10:16:47 +0530

    first commit

---
+++ src/main/java/com/order/in/service/PaymentClient.java
@@ -0,0 +1,14 @@
+package com.order.in.service;
+
+import com.order.in.common.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "PAYMENT-SERVICE",url = "http://13.235.71.157:9092")
+public interface PaymentClient {
+
+    @PostMapping(value = "/payment/makePayment")
+    Payment makePayment(@RequestBody Payment payment);
+
+}
